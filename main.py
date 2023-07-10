6a3ea8f40c7d58576366092f97c49e4c31f05172
CT4111 <117856677+CT4111@users.noreply.github.com>
2023-07-10 12:36:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 from pytube import YouTube
 from tkinter import *
 import os
